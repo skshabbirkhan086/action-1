74c2392d1a29293ea4362ef83b7904ce1febb0cd
Shabbir <kshabbir935@gmail.com>
2026-07-09 19:30:18 +0530

    new workflow

---
+++ shell-script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "hello how are you"
+ls -ltr
+cat README.md
+whoami
+uname
+id
+sudo apt-get install cowsay -y
+cowsay -f dragon "Run for cover, I am a DRAGON...RAWR" >> dragon.txt
+cat dragon.txt
